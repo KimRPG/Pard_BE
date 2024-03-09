074d8eff18b926303089de5f6548066d25a0b846
KimRPG <hunsung9909@gmail.com>
2024-03-10 00:22:09 +0900

    ✨ feat(schedule): 일정 응답 DTO 추가

---
+++ src/main/java/com/pard/pard_backend/domain/schedule/dto/response/ScheduleResponseDTO.java
@@ -0,0 +1,27 @@
+package com.pard.pard_backend.domain.schedule.dto.response;
+
+import com.pard.pard_backend.domain.schedule.entity.Schedule;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ScheduleResponseDTO {
+    private String title;
+    private Date scheduleDate;
+    private String content;
+    private String part;
+
+    public ScheduleResponseDTO(final @NotNull Schedule schedule) {
+        this.title = schedule.getTitle();
+        this.scheduleDate = schedule.getScheduleDate();
+        this.content = schedule.getContent();
+        this.part = schedule.getPart();
+    }
+
+
+}
