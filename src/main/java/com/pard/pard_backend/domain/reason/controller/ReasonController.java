954e723a1ee8839717cb9a0041d547a681135008
Giyoul <cla12093@gmail.com>
2024-07-18 21:34:23 +0900

    📝 docs(reason): 상벌점 사유 API 설명 추가
    Update Reason Controller

--- src/main/java/com/pard/pard_backend/domain/reason/controller/ReasonController.java
+++ src/main/java/com/pard/pard_backend/domain/reason/controller/ReasonController.java
@@ -5,6 +5,7 @@ import com.pard.pard_backend.domain.reason.dto.response.ReasonResponseDTO;
 import com.pard.pard_backend.domain.reason.service.ReasonService;
 import com.pard.pard_backend.domain.security.jwt.JWTUtil;
 import com.pard.pard_backend.domain.user.repository.UserJDBC;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,7 @@ public class ReasonController {
 
 //    유저의 이메일로 점수 / 벌점 추가
    @PostMapping("")
+   @Operation(summary = "해당 이메일의 유저의 상, 벌점을 추가해줍니다.", description = "상, 벌점을 입력한 값으로 수정이 아닌, 입력한 값 만큼 더해줍니다.")
    public void addPoint(@RequestBody ReasonRequest.ReasonRequestDTO req){
        reasonService.addPoint(req);
    }
@@ -27,17 +29,20 @@ public class ReasonController {
 
 //    유저의 이메일로 점수 삭제
     @DeleteMapping("")
+    @Operation(summary = "해당 아이디의 상, 벌점을 삭제합니다.", description = "점수 삭제")
     public void deletePoint(@RequestBody ReasonRequest.ReasonDeleteDTO req){
         reasonService.deletePoint(req);
     }
 
 //    유저의 이메일로 점수 조회 + 팡욱이 점수
     @GetMapping("")
+    @Operation(summary = "토큰의 사용자의 점수를 조회합니다.", description = "입력한 토큰의 사용자를 검색하고, 해당 사용자의 점수를 조회해 줍니다.")
     public List<ReasonResponseDTO.ReasonDTO> getPoint(@CookieValue(value = "Authorization") String token){
         return reasonService.getReason(jwtUtil.getEmail(token));
     }
 
     @GetMapping("/admin")
+    @Operation(summary = "입력한 이메일의 사용자의 점수를 조회합니다.", description = "입력한 사용자의 점수와 이유 등을 반환해줍니다.")
     public List<ReasonResponseDTO.ReasonDTO> getUserPoint(@RequestParam String email) {
         return reasonService.getReason(email);
     }
