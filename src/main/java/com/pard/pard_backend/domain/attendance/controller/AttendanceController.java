7e02422f72a6c028e2427867fe9e3c4c2ba25a8d
Giyoul <cla12093@gmail.com>
2024-07-21 15:01:44 +0900

    📝 docs(api): 출석 및 FCM과 프로젝트 및 QR API 설명 추가

--- src/main/java/com/pard/pard_backend/domain/attendance/controller/AttendanceController.java
+++ src/main/java/com/pard/pard_backend/domain/attendance/controller/AttendanceController.java
@@ -4,6 +4,7 @@ import com.pard.pard_backend.domain.attendance.dto.AttendanceAdminRequestDTO;
 import com.pard.pard_backend.domain.attendance.dto.AttendanceResponseDTO;
 import com.pard.pard_backend.domain.attendance.dto.UserAttendanceResponseDTO;
 import com.pard.pard_backend.domain.attendance.service.AttendanceService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,18 +17,21 @@ public class AttendanceController {
     private final AttendanceService attendanceService;
 
     @PostMapping("/admin")
+    @Operation(summary = "사용자의 출석체크 해줍니다.", description = "입력한 정보대로 유저의 출석 정보를 수정해줍니다.")
     public String checkAttendance(@RequestBody List<AttendanceAdminRequestDTO> dto) {
         attendanceService.postListAttendance(dto);
         return "출첵 확인";
     }
 
     @GetMapping("")
+    @Operation(summary = "입력한 쿠키의 사용자의 모든 출석 정보를 불러옵니다.", description = "쿠키를 입력하면 해당 입력 사용자의 지금까지의 출석 정보를 배열로 반환해줍니다.")
     public List<AttendanceResponseDTO> checkAttendance(@CookieValue("Authorization") String token) {
         return attendanceService.getAttendance(token);
     }
 
     //admin으로 박아놓기
     @GetMapping("/all/{generation}")
+    @Operation(summary = "입력한 기수의 출석 정보를 불러옵니다.", description = "기수의 이름을 입력해주면, 해당 기수의 유저 정보와, 출석 정보를 담아서 반환해줍니다.")
     public List<UserAttendanceResponseDTO> checkAllAttendance(@PathVariable String generation) {
         return attendanceService.getAllAttendance(generation);
     }
--- src/main/java/com/pard/pard_backend/domain/qr/controller/QRController.java
+++ src/main/java/com/pard/pard_backend/domain/qr/controller/QRController.java
@@ -5,6 +5,7 @@ import com.pard.pard_backend.domain.qr.dto.response.ResponseQrDto;
 import com.pard.pard_backend.domain.qr.service.QRService;
 import com.pard.pard_backend.domain.reason.dto.request.ReasonRequest;
 import com.pard.pard_backend.domain.reason.service.ReasonService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,7 @@ public class QRController {
     private final QRService qrService;
 
     @PostMapping("/validQR")
+    @Operation(summary = "입력한 쿠키 사용자가 qr을 찍으면 해당 qr이 파드의 유효 qr인지 확인하고, 출석체크", description = "해당 사용자의 해당 세미나 attendance 체크해줍니다.")
     public ResponseQrDto.attendaceResponse validQR(@RequestBody RequestQrDto.QRAttendanceRequestDTO qrAttendanceRequestDTO,@CookieValue(value = "Authorization") String token){
         return qrService.checkQR(qrAttendanceRequestDTO,token);
     }
